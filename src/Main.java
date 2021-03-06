31d848a18c92ca858cf334e824e37b5c6e398237
zheleznovux <66991410+zheleznovux@users.noreply.github.com>
2021-03-06 08:57:31 +0700

    ///.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 
 public class Main {
     public static void main(String args[])  {
-        System.out.print("Hello\n");
-        Sedan volvoS80 = new Sedan("VolvoS80", "Red", 160, "rear-wheel");
-        System.out.println(volvoS80.getDescription());
+//        System.out.print("Hello\n");
+//        Sedan volvoS80 = new Sedan("VolvoS80", "Red", 160, "rear-wheel");
+//        System.out.println(volvoS80.getDescription());
         Offroader toyotaFJCruiser = new Offroader("ToyotaFJCruiser","yellow", 200, false);
         System.out.println(toyotaFJCruiser.getDescription());
+        SuperCar lambo = new SuperCar("Lambo","red",250,"rear-wheel",3.5);
+        System.out.println(lambo.getDescription());
     }
-
 }
