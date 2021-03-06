0b95e3902f629c2a607e799872c9286703638c8f
zheleznovux <66991410+zheleznovux@users.noreply.github.com>
2021-03-06 09:42:37 +0700

    ///.

--- src/lab1.java
+++ src/lab1.java
@@ -10,22 +10,22 @@ public class lab1 {
 //        cars.Sedan volvoS80 = new cars.Sedan("VolvoS80", "Red", 160, "rear-wheel");
 //        System.out.println(volvoS80.getDescription());
         Random random = new Random();
-        int i = random.nextInt(2) + 1;
 
-        for(; i != 0; i--) {
-            switch (i) {
-                case (1) -> {
+        for(int i = 0; i <= 9; i++) {
+            int j = random.nextInt(2) + 1;
+            switch (j) {
+                case (1):
                     Offroader toyotaFJCruiser = new Offroader("ToyotaFJCruiser", "yellow", 200, false);
                     System.out.println(toyotaFJCruiser.getDescription());
-                }
-                case (2) -> {
+                    break;
+                case (2):
                     SuperCar lambo = new SuperCar("Lambo", "red", 250, "rear-wheel", 3.5);
                     System.out.println(lambo.getDescription());
-                }
-                case (3) -> {
+                    break;
+                case (3):
                     AudiTT ttsNumber = new AudiTT("grey", 220, "rear-wheel", 4.6);
                     System.out.println(ttsNumber.getDescription());
-                }
+                    break;
             }
 
         }
